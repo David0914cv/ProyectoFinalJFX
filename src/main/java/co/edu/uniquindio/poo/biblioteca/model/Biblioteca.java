0ca13957447a30a051502b540733ba823f108b95
EstebanPareja <juane.parejag@uqvirtual.edu.co>
2025-05-20 22:52:13 -0500

    Avance codes

--- src/main/java/co/edu/uniquindio/poo/biblioteca/model/Biblioteca.java
+++ src/main/java/co/edu/uniquindio/poo/biblioteca/model/Biblioteca.java
@@ -31,21 +31,187 @@ public class Biblioteca {
         return resultado;
     }
 
-    public void agregarUsuario(Usuario usuario) {
-        listUsuarios.add(usuario);
+    public boolean verificarUsuarioExiste(Usuario usuario) {
+        for (Usuario user : listUsuarios) {
+            if(usuario.getNumeroIdentificacion().equals(user.getNumeroIdentificacion())) {
+                return true;
+            }
+        } return false;
     }
 
-    public void agregarDevolucion(Devolucion devolucion) {
+    public boolean agregarUsuario(Usuario usuario) {
+        if (verificarUsuarioExiste(usuario)) {
+            return false;
+        }
+        else {
+            listUsuarios.add(usuario);
+            return true;
+        }
+    }
+
+    public boolean eliminarUsuario(String numeroIdentificacionUsuario) {
+        for (int i = 0; i < listUsuarios.size(); i++) {
+            if(listUsuarios.get(i).getNumeroIdentificacion().equals(numeroIdentificacionUsuario)) {
+                listUsuarios.remove(i);
+                return true;
+            }
+        } return false;
+    }
+
+    public boolean actualizarUsuario(String numeroIdentificacionUsuario, Usuario usuario) {
+        for (int i = 0; i < listUsuarios.size(); i++) {
+            if(listUsuarios.get(i).getNumeroIdentificacion().equals(numeroIdentificacionUsuario)) {
+                listUsuarios.set(i, usuario);
+                return true;
+            }
+        } return false;
+    }
+
+    public Usuario mostrarUsuario(String numeroIdentificacionUsuario) {
+        for (Usuario user : listUsuarios) {
+            if(user.getNumeroIdentificacion().equals(numeroIdentificacionUsuario)) {
+                return user;
+            }
+        } return null;
+    }
+
+    public boolean verificarDevolucionExiste(Devolucion devolucion) {
+        for (Devolucion devolucion1 : listDevoluciones) {
+            if(devolucion.getId().equals(devolucion1.getId())) {
+                return true;
+            }
+        } return false;
+    }
+
+
+    public boolean agregarDevolucion(Devolucion devolucion) {
+        if (verificarDevolucionExiste(devolucion)) {
+            return false;
+        }
         listDevoluciones.add(devolucion);
+        return true;
     }
 
-    public void agregarPrestamo(Prestamo prestamo) {
-        listPrestamos.add(prestamo);
+    public boolean eliminarDevolucion (String idDevolucion) {
+        for (int i = 0; i < listDevoluciones.size(); i++) {
+            if(listDevoluciones.get(i).getId().equals(idDevolucion)) {
+                listDevoluciones.remove(i);
+                return true;
+            }
+        } return false;
     }
 
-    public void agregarLibro(Libro libro) {
-        listLibros.add(libro);
+    public boolean actualizarDevolucion (String idDevolucion, Devolucion devolucion) {
+        for (int i = 0; i < listDevoluciones.size(); i++) {
+            if(listDevoluciones.get(i).getId().equals(idDevolucion)) {
+                listDevoluciones.set(i, devolucion);
+                return true;
+            }
+        } return false;
     }
+
+    public Devolucion mostrarDevolucion(String idDevolucion) {
+        for (Devolucion devolucion1 : listDevoluciones) {
+            if(devolucion1.getId().equals(idDevolucion)) {
+                return devolucion1;
+            }
+        } return null;
+    }
+
+    public boolean verificarPrestamoExiste(Prestamo prestamo) {
+        for (Prestamo prestamo1 : listPrestamos) {
+            if (prestamo.getId().equals(prestamo1.getId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean agregarPrestamo(Prestamo prestamo) {
+        if (verificarPrestamoExiste(prestamo)) {
+            return false;
+        } else {
+            listPrestamos.add(prestamo);
+            return true;
+        }
+    }
+
+    public boolean eliminarPrestamo(String idPrestamo) {
+        for (int i = 0; i < listPrestamos.size(); i++) {
+            if (listPrestamos.get(i).getId().equals(idPrestamo)) {
+                listPrestamos.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualizarPrestamo(String idPrestamo, Prestamo prestamo) {
+        for (int i = 0; i < listPrestamos.size(); i++) {
+            if (listPrestamos.get(i).getId().equals(idPrestamo)) {
+                listPrestamos.set(i, prestamo);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Prestamo mostrarPrestamo(String idPrestamo) {
+        for (Prestamo p : listPrestamos) {
+            if (p.getId().equals(idPrestamo)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public boolean verificarLibroExiste(Libro libro) {
+        for (Libro book : listLibros) {
+            if (libro.getCodigo().equals(book.getCodigo())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean agregarLibro(Libro libro) {
+        if (verificarLibroExiste(libro)) {
+            return false;
+        } else {
+            listLibros.add(libro);
+            return true;
+        }
+    }
+
+    public boolean eliminarLibro(String codigoLibro) {
+        for (int i = 0; i < listLibros.size(); i++) {
+            if (listLibros.get(i).getCodigo().equals(codigoLibro)) {
+                listLibros.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualizarLibro(String codigoLibro, Libro libro) {
+        for (int i = 0; i < listLibros.size(); i++) {
+            if (listLibros.get(i).getCodigo().equals(codigoLibro)) {
+                listLibros.set(i, libro);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Libro mostrarLibro(String codigoLibro) {
+        for (Libro l : listLibros) {
+            if (l.getCodigo().equals(codigoLibro)) {
+                return l;
+            }
+        }
+        return null;
+    }
+
 
     public String getNombre() {
         return nombre;
--- src/main/java/co/edu/uniquindio/poo/biblioteca/model/CrearReporte.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uniquindio.poo.biblioteca.model;
-
-public interface CrearReporte {
-
-    public void crearReporte();
-}
